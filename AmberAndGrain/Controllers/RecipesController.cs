ca3a167d4ca3853679c06271ccc4bc326e224682
Gina <ginamantonini@gmail.com>
2018-04-03 20:44:39 -0500

    project setup and controller for create recipe done

---
+++ AmberAndGrain/Controllers/RecipesController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using AmberAndGrain.Models;
+using AmberAndGrain.Services;
+
+namespace AmberAndGrain.Controllers
+{
+    [RoutePrefix("api/recipes")]
+    public class RecipesController : ApiController
+    {
+        [Route(""), HttpPost]
+        public HttpResponseMessage AddRecipe (RecipeDTO recipe)
+        {
+            var repository = new RecipeRepository();
+            var result = repository.Create(recipe);
+            if (result)
+            {
+                return Request.CreateResponse(HttpStatusCode.Created);
+            }
+
+            return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, "Could not create Recipe. Please try again");
+        }
+    }
+
+}
---
+++ AmberAndGrain/Models/RecipeDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AmberAndGrain.Models
+{
+    public class RecipeDTO
+    {
+        public string name { get; set; }
+        public decimal percentWheat { get; set; }
+        public decimal percentCorn { get; set; }
+        public int barrelAge { get; set; }
+        public string barrelMaterial { get; set; }
+        public string creator { get; set; }
+    }
+}
